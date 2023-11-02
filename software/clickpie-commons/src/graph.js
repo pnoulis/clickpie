89c6657f6c18bc00916da5e2e3472bcde78de037
pavlos noulis <pavlos.noulis@gmail.com>
2023-11-02 04:12:05 +0200

    Added support for creating graphs*
    A graph will be used to represent, manipulate and cache clickups
    hierarchy composed of:
    
    1. Account
    2. Workspace#1, ..., N
    3. Workspace.space#1, ..., N
    4. Workspace.space.folder#1, ..., N
    5. Workspace.space.folder.list#1, ..., N
    6. Workspace.space.folder.list.task#1, ..., N
    7. Workspace.space.list#1, ..., N

---
+++ software/clickpie-commons/src/graph.js
@@ -0,0 +1,95 @@
+/*
+  An Adjacency-list tree is one solution of many to the question:
+
+  How might one represent a tree, directed or undirected using
+  a programming language as the means of expression?
+
+  The adjacency-list representation of a graph G = (V,E) consists of an array
+  *Adj* of |V| lists, one for each vertex in V. For each u e V, the adjacency
+  list Adj[u] contains all vertices v such that there is an edge (u, v) e E.
+  Thatis, Adj[u] consists of all the vertices adjacent to u in G.
+  (Introduction to algorithms, Thomas H. Cormen, 3rd ed)
+
+  As an abstraction, an Adjacency-list representation is well defined. However
+  implementing it necessiates technical choices which drive the complexity,
+  speed,ease of use, effectiveness and even allowed operations of the resulting
+  structure
+
+  [USAGE]
+
+  const graph = new Graph();
+  const root = 'root';
+  const vertexA = { id: "somid", ...props };
+  const vertexB = { id: "nodid", ...props };
+
+  graph.addVertex(root);
+  graph.addVertex(vertexA);
+  graph.addVertex(vertexB);
+
+  graph.addEdge(root, vertexA);
+  graph.addEdge(root, vertexB);
+
+  graph.traverse(root, (vertex, stop) => {
+  if (vertex.id = 'somid') stop();
+  })
+
+*/
+
+class Graph {
+  /* verteces: u, v, w */
+  /* edges: e, f */
+  /* source vertex in BFS: s */
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+  addVertex(u) {
+    this.adjacencyList.set(u, new Set());
+  }
+  addEdge(u, v) {
+    this.adjacencyList.get(u).add(v);
+    this.adjacencyList.get(v).add(u);
+  }
+  getNeighboors(u) {
+    return this.adjacencyList.get(u);
+  }
+  hasEdge(u, v) {
+    return this.adjacencyList.get(u).has(v);
+  }
+  hasVertex(u) {
+    return this.adjacencyList.has(u);
+  }
+  traverse(s, cb) {
+    const V = this.adjacencyList.length;
+    const visited = new Map();
+    const queue = [];
+    const stop = function () {
+      queue.length = 0;
+    };
+    if (typeof cb !== "function") {
+      cb = function (vertext, stop) {};
+    }
+
+    for (let i = 0; i < V; i++) {
+      visited[i] = false;
+    }
+
+    visited.set(s, true);
+    queue.push(s);
+
+    while (queue.length > 0) {
+      s = queue[0];
+      console.log(s);
+      queue.shift();
+
+      for (const vertex of this.getNeighboors(s)) {
+        if (!visited.has(vertex)) {
+          visited.set(vertex, true);
+          queue.push(vertex);
+        }
+      }
+      cb(s, stop);
+    }
+  }
+}
+
+export { Graph };
